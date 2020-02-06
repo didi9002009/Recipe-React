0811c0aa79f36437ab524d0b1a7c054d37015059
didi9002009 <53850889+didi9002009@users.noreply.github.com>
2020-02-06 16:45:12 +1100

    Update final

--- src/App.js
+++ src/App.js
@@ -7,35 +7,55 @@ const App = () => {
   const APP_KEY = "be55ccb7eddc846f3f40b219db0f4895";
 
   const [recipes, setRecipes] = useState([]);
+  const [search, setSearch] = useState("");
+  const [query, setQuery] = useState("chicken");
 
   useEffect(() => {
     getRecipes();
-  }, []);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [query]);
   // 1. taking things from state below
   const getRecipes = async () => {
     const response = await fetch(
-      `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`
+      `https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`
     );
     const data = await response.json();
     setRecipes(data.hits);
   };
 
+  const updateSearch = e => {
+    setSearch(e.target.value);
+  };
+  const getSearch = e => {
+    e.preventDefault();
+    setQuery(search);
+    setSearch("");
+  };
   return (
     <div className="App">
-      <form className="search-form">
-        <input className="search-bar" type="text" />
+      <form onSubmit={getSearch} className="search-form">
+        <input
+          className="search-bar"
+          type="text"
+          value={search}
+          onChange={updateSearch}
+        />
         <button className="search-button" type="submit">
           Search
         </button>
       </form>
-      {recipes.map(recipe => (
-        // 2 passing them down to the props.(return html, use () instead of {}) and passing to Recipe component
-        <Recipe
-          title={recipe.recipe.label}
-          calories={recipe.recipe.calories}
-          image={recipe.recipe.image}
-        />
-      ))}
+      <div className="recipes">
+        {recipes.map(recipe => (
+          // 2 passing them down to the props.(return html, use () instead of {}) and passing to Recipe component
+          <Recipe
+            key={recipe.recipe.label}
+            title={recipe.recipe.label}
+            calories={recipe.recipe.calories}
+            image={recipe.recipe.image}
+            ingredients={recipe.recipe.ingredients}
+          />
+        ))}
+      </div>
     </div>
   );
 };
--- src/Recipe.js
+++ src/Recipe.js
@@ -1,12 +1,19 @@
 import React from "react";
+import style from "./recipe.module.css";
 
 //3. pass them doown to the component
-const Recipe = (title, calories, image) => {
+const Recipe = ({ title, calories, image, ingredients = [] }) => {
+  //{title ... } deconstruction
   return (
-    <div>
+    <div className={style.recipe}>
       <h1>{title}</h1>
+      <ol>
+        {ingredients.map(ingredient => (
+          <li>{ingredient.text}</li>
+        ))}
+      </ol>
       <p>{calories}</p>
-      <img src={image} alt="" />
+      <img className={style.image} src={image} alt="" />
     </div>
   );
 };
